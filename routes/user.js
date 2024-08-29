3b59e3610352f961035d7a590269446d3cd94b38
Max Allan-Smith <Maxibon_1195@hotmail.com>
2024-08-29 22:37:57 +0100

    Added Some Logic to GET and POST User

--- routes/user.js
+++ routes/user.js
@@ -1,8 +1,37 @@
 const express = require('express');
 const router = express.Router();
 
+let users = [];
+
+// GET all users
 router.get('/', (req, res) => {
-  res.send('Hello World');
+  res.json(users);
+});
+
+// POST a new user
+router.post('/', (req, res) => {
+  const userData = req.body;
+
+  // Validation: Check if username and password are provided
+  if (!userData.username) {
+    return res.status(400).json({ error: 'Username is required' });
+  }
+  
+  if (!userData.password) {
+    return res.status(400).json({ error: 'Password is required' });
+  }
+
+  // Create a new user object
+  const newUser = {
+    username: userData.username,
+    password: userData.password
+  };
+
+  // Add the new user to the users array
+  users.push(newUser);
+
+  // Respond with the created user
+  res.status(201).json(newUser);
 });
 
 module.exports = router;
